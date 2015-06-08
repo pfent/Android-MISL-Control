f34ff1f343680b0b61c9f327d2f181a8b78edfd2
bsautermeister <apps@bsautermeister.de>
2015-06-08 23:57:47 +0200

    Added Google Play Services legal notices dialog. This one is currently not used, but should be displayed from the about page later on.

--- app/src/main/java/de/tum/in/mislcontrol/dialogs/PlayServicesLegalNoticesDialogFragment.java
+++ app/src/main/java/de/tum/in/mislcontrol/dialogs/PlayServicesLegalNoticesDialogFragment.java
@@ -8,43 +8,42 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import de.tum.in.mislcontrol.MainActivity;
 import de.tum.in.mislcontrol.R;
 import de.tum.in.mislcontrol.SettingsActivity;
 
 /**
- * The dialog fragment for the setup wizard to guide the user on how to connect to the ASEP robot.
+ * The dialog fragment to display the "Legal Notices" which is required
+ * whenever an app uses the Google Play Services.
  */
-public class SetupWizardDialogFragment extends DialogFragment {
+public class PlayServicesLegalNoticesDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        // Get the layout inflater
+        // get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // set legal notices text
+        View view = inflater.inflate(R.layout.fragment_dialog_play_services_legal_notices, null);
+        TextView legalNotesTextView = (TextView)view.findViewById(R.id.play_services_legal_notes_content);
+        legalNotesTextView.setText(GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(getActivity().getApplicationContext()));
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.fragment_dialog_setup_wizard, null))
-                // Add action buttons
+        builder.setTitle(R.string.play_services_legal_notices_title)
+                .setView(inflater.inflate(R.layout.fragment_dialog_play_services_legal_notices, null))
+                // add action buttons
                 .setPositiveButton(R.string.setup, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        SetupWizardDialogFragment.this.getDialog().cancel();
-
-                        // forward to settings activity
-                        Intent intent = new Intent(getActivity(), SettingsActivity.class);
-                        startActivity(intent);
-                    }
-                })
-                .setNegativeButton(R.string.skip, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        SetupWizardDialogFragment.this.getDialog().cancel();
-
-                        // forward to main activity
-                        Intent intent = new Intent(getActivity(), MainActivity.class);
-                        startActivity(intent);
+                        // just close it
+                        PlayServicesLegalNoticesDialogFragment.this.getDialog().cancel();
                     }
                 });
         return builder.create();
